c7753a208b56f21fcade7a284b05eb70b6899c0d
Dan Hintea Qubiz <dan.hintea@qubiz.com>
2025-12-03 13:51:51 +0200

    Adapt raw data from Azure TS to proper Mongo Data Type
    + Update Docs

--- Src/H.Play.HttpGate/H.Replication.MongoDB/AzureTableStorage/AzureTableStorageParsingXtnx.cs
+++ Src/H.Play.HttpGate/H.Replication.MongoDB/AzureTableStorage/AzureTableStorageParsingXtnx.cs
@@ -3,6 +3,7 @@ using H.Replication.Contracts.DataContracts;
 using MongoDB.Bson;
 using MongoDB.Bson.Serialization;
 using System.Text.RegularExpressions;
+using System.Xml;
 
 namespace H.Replication.MongoDB.AzureTableStorage
 {
@@ -85,19 +86,87 @@ namespace H.Replication.MongoDB.AzureTableStorage
                     PartitionKey = idParts[1],
                     RowKey = idParts[2],
                     ReplicationOperation = replicationRequest.Operation,
-                    Document = replicationRequest.Payload.MorphIfNotEmpty(x => BsonSerializer.Deserialize<BsonDocument>(x.RemoveODataMetaProps()), defaultTo: null),
+                    Document = replicationRequest.Payload.MorphIfNotEmpty(x => BsonSerializer.Deserialize<BsonDocument>(x.PreProcessJsonForBsonDocument()).PostProcessBsonDocument(), defaultTo: null),
                 }
                 ;
         }
 
+        static BsonDocument PostProcessBsonDocument(this BsonDocument bson)
+        {
+            if (bson is null)
+                return bson;
+
+            return
+                [.. bson.Select(x => new BsonElement(
+                    x.Name,
+                    x.Value.PostProcessBsonValue()
+                ))];
+        }
+
+        static BsonValue PostProcessBsonValue(this BsonValue bsonValue)
+        {
+            if (bsonValue is null)
+                return BsonValue.Create(null);
+
+            if (bsonValue.IsString)
+                return bsonValue.AsString.PostProcessBsonValue();
+
+            if (bsonValue.IsBsonDocument)
+                return bsonValue.AsBsonDocument.PostProcessBsonDocument();
+
+            if (bsonValue.IsBsonArray)
+                return BsonValue.Create(bsonValue.AsBsonArray.Select(x => x.PostProcessBsonValue()));
+
+            return bsonValue;
+        }
+
+        static BsonValue PostProcessBsonValue(this string jsonValue)
+        {
+            if (jsonValue.IsEmpty())
+                return BsonValue.Create(null);
+
+            if (DateTime.TryParse(jsonValue, null, System.Globalization.DateTimeStyles.RoundtripKind, out var asDateTime))
+                return new BsonDateTime(asDateTime);
+
+            if (Guid.TryParse(jsonValue, out var asGuid))
+                return new BsonBinaryData(asGuid, GuidRepresentation.Standard);
+
+            if (HSafe.Run(() => XmlConvert.ToTimeSpan(jsonValue)).RefPayload(out var asTimespan))
+                return new BsonString(asTimespan.ToString());
+
+            return new BsonString(jsonValue);
+        }
+
+
+
+        static string PreProcessJsonForBsonDocument(this string json)
+        {
+            if (json.IsEmpty())
+                return json;
+
+            return
+                json
+                .RemoveODataMetaProps()
+                .PreProcessNumericValues()
+                ;
+        }
+
         static readonly Regex oDataMetaPropsRegex = new Regex(",\"[^\"]+@odata\\.type\":\"[^\"]+\"");
-        static readonly Regex numericPropValuePropsRegex = new Regex(":\"(\\d+.?\\d*)\"");
         static string RemoveODataMetaProps(this string json)
         {
             if (json.IsEmpty())
                 return json;
             string result = json;
             result = oDataMetaPropsRegex.Replace(result, string.Empty);
+            return result;
+        }
+
+        static readonly Regex numericPropValuePropsRegex = new Regex(":\"(\\d+.?\\d*)\"");
+        static string PreProcessNumericValues(this string json)
+        {
+            if (json.IsEmpty())
+                return json;
+            string result = json;
             result = numericPropValuePropsRegex.Replace(result, ":$1");
             return result;
         }
